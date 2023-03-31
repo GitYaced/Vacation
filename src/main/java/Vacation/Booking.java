3849f42b3dcbfd212bfe1061e7de15f90ff7b16a
ricky <rickybreinburg@yahoo.com>
2023-03-31 14:54:25 +0200

    booking class vars added

--- src/main/java/Vacation/Booking.java
+++ src/main/java/Vacation/Booking.java
@@ -1,5 +1,19 @@
 package Vacation;
 
-class Booking extends Vacation{
+import java.util.Date;
+
+class Booking extends Vacation {
+
+    int bookingID;
+    Date bookingDate;
+    String customerName;
+    String customerEmail;
+    String bookingStatus;
+    String paymentStatus;
+    String bookingType;
+    String bookingDetails;
+    double bookingCost;
+    String paymentMethod;
+
 
 }
